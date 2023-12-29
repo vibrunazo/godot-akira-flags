8aa9468d90e6b9b96e4d31c79c5410fcb5e7638d
vib <vibrunazo@gmail.com>
2023-12-29 19:19:48 -0300

    end anim

--- quiz/quiz.gd
+++ quiz/quiz.gd
@@ -79,6 +79,7 @@ func _on_flag_pressed(button: FlagButton):
 func play_correct():
 	audio_yes.play()
 	await audio_yes.finished
+	anim.play('end')
 	await get_tree().create_timer(0.7).timeout
 	get_tree().change_scene_to_file("res://quiz/quiz.tscn")
 
