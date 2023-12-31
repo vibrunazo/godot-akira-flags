faf18d65a0fd43df73622223c83892996b6c5a35
vib <vibrunazo@gmail.com>
2023-12-31 16:20:29 -0300

    sync button click audio

--- quiz/flag_button.gd
+++ quiz/flag_button.gd
@@ -3,10 +3,16 @@ extends Button
 class_name FlagButton
 
 @export var country_id: String = 'brazil'
+@onready var audio: AudioStreamPlayer = $AudioClick
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	icon = Countries.get_flag(country_id)
 
 func _on_pressed():
-	$AudioClick.play()
+	audio.volume_db = -12
+	audio.play()
+
+func _on_button_down():
+	audio.volume_db = 0
+	audio.play()
