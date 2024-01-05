ab54d638a11a8f492d6293e9abd6b746e9f5785d
vib <vibrunazo@gmail.com>
2024-01-05 11:37:03 -0300

    resize flag inside ball

--- games/ballgame/player_ball.gd
+++ games/ballgame/player_ball.gd
@@ -8,6 +8,7 @@ class_name PlayerBall extends RigidBody2D
 
 @onready var audio: AudioStreamPlayer2D = $AudioStreamPlayer2D
 @onready var sprite_flag: Sprite2D = %SpriteFlag
+@onready var sprite_flag2: Sprite2D = %SpriteFlag2
 
 
 # Called when the node enters the scene tree for the first time.
@@ -29,4 +30,5 @@ func _on_body_entered(_body):
 
 func update_country():
 	sprite_flag.texture = Countries.get_flag(team)
+	sprite_flag2.texture = Countries.get_flag(team)
 	
