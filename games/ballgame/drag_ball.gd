999f1fe18c51f3ede5d64c5926cc9db9eecec8cf
vib <vibrunazo@gmail.com>
2024-01-05 21:28:21 -0300

    show particle fx on drag

--- games/ballgame/drag_ball.gd
+++ games/ballgame/drag_ball.gd
@@ -1,6 +1,7 @@
 ## Ball that gets dragged by the mouse to move other balls
 class_name DragBall extends RigidBody2D
 
+@export var fx_scene: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,20 +9,12 @@ func _ready():
 
 func _physics_process(delta):
 	var dir: Vector2 = get_global_mouse_position() - global_position
-		#return
-	#dir = dir.normalized()
 	var vel: Vector2 = dir * delta * 500
-	#move_and_collide(vel)
-	#apply_force(vel)
 	global_position = get_global_mouse_position()
-	#global_position += vel
 	linear_velocity = vel
 	if dir.length() < 10: 
 		linear_velocity = Vector2.ZERO
 	
-
-
-
 
 func _on_area_2d_body_entered(body):
 	if linear_velocity.length() < 10: return
@@ -33,3 +26,11 @@ func _on_area_2d_body_entered(body):
 		body.apply_impulse(dir * power + linear_velocity)
 		if body.has_method("play_hit"):
 			body.play_hit(power * 0.7)
+
+
+func _on_timer_timeout():
+	print('moo')
+	var fx: GPUParticles2D = fx_scene.instantiate() as GPUParticles2D
+	fx.amount_ratio = 0.2
+	fx.global_position = global_position
+	get_parent().add_child(fx)
